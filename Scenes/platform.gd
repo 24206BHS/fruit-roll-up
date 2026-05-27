34c52359d5b656a4e93181a812cf3a0456fd5b9d
24206BHS <24206@burnside.school.nz>
2026-05-27 12:45:42 +1200

    added rotation to platform

--- Scenes/platform.gd
+++ Scenes/platform.gd
@@ -1,15 +1,17 @@
 extends StaticBody2D
 
+var moving = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if moving:
+		pass
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	print(viewport, event, shape_idx)
-	if event is InputEventMouseMotion and event.button_mask == 1:
-		position += event.relative
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			moving = true
+		elif event.button_index == 4:
+			rotate(-PI/24)
+		elif event.button_index == 5:
+			rotate(PI/24)
