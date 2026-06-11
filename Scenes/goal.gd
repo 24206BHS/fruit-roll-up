c59a0a6997b1d2e4b71715944e4995f89b481ab5
24206BHS <24206@burnside.school.nz>
2026-06-12 09:25:57 +1200

    tweaked goal sizes and made second level but platforms don't work yet in second level

--- Scenes/goal.gd
+++ Scenes/goal.gd
@@ -6,7 +6,5 @@ func _ready() -> void:
 	modulate = Color(256,0,0)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("a")
 	if body.is_in_group("Fruit"):
-		print("b")
 		modulate = Color(0,256,0)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -7,6 +7,7 @@ var isDropped = false
 func _ready() -> void:
 	dropPos = $Dropper.position
 	dropPos.y += 20
+	$Platform.get_signal_list()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
