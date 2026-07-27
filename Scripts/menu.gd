dfc4c9e57de6e69fd33f7082213eff2ac40894b0
24206BHS <24206@burnside.school.nz>
2026-07-27 12:06:08 +1200

    added main menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,11 +1,7 @@
 extends Control
 
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_level_select_button_pressed() -> void:
+	$LevelSelect.visible = true
