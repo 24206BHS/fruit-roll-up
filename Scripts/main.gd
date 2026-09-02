f9f23c50a3219e784c83f64e07e589d7e665dfda
24206BHS <24206@burnside.school.nz>
2026-09-02 12:45:04 +1200

    added level 8

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -8,6 +8,7 @@ var playing = true
 var next = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Control.visible = true
 	$Control/Win.visible = false
 	dropPos = $Dropper.position
 	dropPos.y += 20
